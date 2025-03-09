2554c42c5a613cdccbf2fe4d10543a72d20e187b
Joseph Aycock <josephfelixaycock@outlook.com>
2025-03-09 13:50:02 -0500

    change select floor screen and added logo

--- lib/floor_selection_screen.dart
+++ lib/floor_selection_screen.dart
@@ -8,10 +8,21 @@ class FloorSelectionScreen extends StatelessWidget {
       appBar: AppBar(title: Text("Select a Floor")),
       backgroundColor: Colors.purple, // Purple background
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        padding: const EdgeInsets.fromLTRB(
+          20,
+          20,
+          20,
+          40,
+        ), // Extra bottom padding
         child: Column(
           children: [
-            // Intro text at the top
+            // Logo at the top center
+            Image.asset(
+              'assets/logo.png',
+              height: 100, // Adjust size as needed
+            ),
+            SizedBox(height: 20),
+            // Introductory text
             Expanded(
               child: Center(
                 child: Text(
@@ -20,7 +31,7 @@ class FloorSelectionScreen extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                    color: Colors.white, // White text on purple
                   ),
                 ),
               ),
@@ -31,10 +42,7 @@ class FloorSelectionScreen extends StatelessWidget {
               children: [
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    minimumSize: Size(
-                      double.infinity,
-                      50,
-                    ), // Full-width, height 50
+                    minimumSize: Size(double.infinity, 50), // Full-width button
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(
                         10,
